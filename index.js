83b67d1922660c79936217bfe75732e3fba1ecdc
gdwehsjnk <bsjcurakvemelis@hotmail.com>
2024-05-14 04:27:00 +0700

    test task

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-function uniquePaths(m, n) {
-  const dp = new Array(m).fill(0).map(() => new Array(n).fill(0));
-  for (let i = 0; i < m; i++) {
-    dp[i][0] = 1;
+function singleNumber(nums) {
+  let result = 0;
+  for (const num of nums) {
+    result ^= num;
   }
-  for (let j = 0; j < n; j++) {
-    dp[0][j] = 1;
-  }
-  for (let i = 1; i < m; i++) {
-    for (let j = 1; j < n; j++) {
-      dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-    }
-  }
-  return dp[m - 1][n - 1];
+  return result;
 }
